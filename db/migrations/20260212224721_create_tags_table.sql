f5bac5d09ae70286d59080779de8e9dc52cbdb17
antikytheraton <binauralvoice@gmail.com>
2026-02-13 12:34:05 -0600

    test: revert prev commit

--- db/migrations/20260212224721_create_tags_table.sql
+++ db/migrations/20260212224721_create_tags_table.sql
@@ -2,7 +2,6 @@
 
 CREATE TABLE tags (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id uuid NOT NULL,
     name TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
