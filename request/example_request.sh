5da1ccc6cd96b52938cb0493ebdddf091305ca2a
antikytheraton <16123078+antikytheraton@users.noreply.github.com>
2026-02-12 18:48:27 -0600

    feat: test remote endpoint

--- request/example_request.sh
+++ request/example_request.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-BASE_URL="http://localhost:3000"
+BASE_URL=http://localhost:3000
+BASE_URL=https://halucigenia-backend-995b6e4c7c37.herokuapp.com
 
-curl -X POST $BASE_URL/api/v1/bookmarks \
+curl -sSL \
+  -X POST $BASE_URL/api/v1/bookmarks \
   -H "Content-Type: application/json" \
   -d '{
     "url": "https://example.com",
     "title": "Example"
-  }'
+  }' | jq
